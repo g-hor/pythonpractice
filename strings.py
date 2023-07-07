c687be34f1d23a1bf8df5a308a3c3d9f1974f2b1
Gary <geehor@gmail.com>
2023-07-07 01:27:54 -0400

    Testing string methods

---
+++ strings.py
@@ -0,0 +1,26 @@
+print("Hello world!")
+print("What's up?")
+print('Isn\'t the world a beautiful place?')
+print('''Is this the real life?
+Is this just fantasy?
+Caught in a landslide
+No escape from reality''')
+print(len("woopdeedoo"))
+print("zebra"[0])
+print("zebra"[-3])
+print("zebra"[30])
+print("zebra"[4:4])
+print("zebra"[8:-2])
+print("zebra"[:3])
+print("zebra"[3:])
+
+print("spaghetti".index("t"))
+print("spaghetti".index("x"))
+print("spaghetti".count("s"))
+
+print("gold" + "fish")
+print("ha"*10)
+
+noun = "Mental health"
+adjective = "muy importante"
+print("{0} is {1}! :)".format(noun, adjective))
