832e5ca557e46eb468dff9794eb5a0f36aa34344
Gary <geehor@gmail.com>
2023-07-07 22:27:14 -0400

    Playing with all and any built in functions

---
+++ data_structures/all_any.py
@@ -0,0 +1,14 @@
+titles1 = ['Mr', 'Mrs', 'Ms']
+titles2 = ['Mr', 'Mrs', 'Ms', '']
+titles3 = []
+
+print(all(titles1))
+print(all(titles2))
+print(all(titles3))
+
+feedback1 = ['', '', '', '']
+feedback2 = ['Awesome', '', '' , '']
+feedback3 = []
+print(any(feedback1))
+print(any(feedback2))
+print(any(feedback3))
